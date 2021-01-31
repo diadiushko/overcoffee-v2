ee1e393701e5fd24fc8c20190bfc59017e011b32
fluxick <artem.rzaev.200127@gmail.com>
2021-01-31 16:18:35 +0200

    scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,3 +1,35 @@
-window.addEventListener('mousewheel', (e) => {
-    window.scrollBy(0, 1000)
-})
+const navButtons = document.querySelectorAll("nav li");
+
+const navActive = (number) => {
+    navButtons.forEach((e) => e.classList.remove("active"));
+    navButtons[number].classList.add("active");
+};
+
+document.addEventListener("click", (event) => {
+    if (event.target.dataset.nav) {
+        navActive(parseInt(event.target.dataset.nav));
+    }
+});
+
+const scrollListener = (e) => {
+    document.removeEventListener("mousewheel", scrollListener);
+    let current;
+    navButtons.forEach((e, i) =>
+        e.classList.contains("active") ? (current = i) : void 0
+    );
+    if (e.wheelDelta < 0) {
+        window.scrollBy(0, window.innerHeight);
+        current =
+            current === navButtons.length - 1 ? navButtons.length - 1 : current + 1;
+        navActive(current);
+    } else {
+        window.scrollBy(0, -window.innerHeight);
+        current =
+            current === 0 ? 0 : current - 1;
+        navActive(current);
+    }
+    setTimeout(() => {
+        document.addEventListener("mousewheel", scrollListener);
+    }, 500);
+}
+document.addEventListener("mousewheel", scrollListener);
